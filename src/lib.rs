1e5b1f4613eaaf1fcb090cab2341cfe26703d200
Darren Tsung <darren@onesignal.com>
2018-07-12 13:09:49 -0700

    0.2.0, add ability for different sizes

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,10 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 /// Essentially an AtomicUsize that is clonable and whose count is based
 /// on the number of copies. The count is automatically updated on Drop.
-pub struct Counter(Arc<AtomicUsize>);
+pub struct Counter {
+    counter: Arc<AtomicUsize>,
+    size: usize,
+}
 
 /// A 'weak' Counter that does not affect the count.
 #[derive(Clone)]
@@ -40,33 +43,40 @@ pub struct WeakCounter(Arc<AtomicUsize>);
 
 impl Counter {
     pub fn new() -> Counter {
-        Counter(Arc::new(AtomicUsize::new(1)))
+        Counter::new_with_size(1)
+    }
+
+    pub fn new_with_size(size: usize) -> Counter {
+        Counter {
+            counter: Arc::new(AtomicUsize::new(1)),
+            size,
+        }
     }
 
     /// Consume self (causing the count to decrease by 1)
     /// and return a weak reference to the count through a WeakCounter
     pub fn downgrade(self) -> WeakCounter {
-        WeakCounter(self.0.clone())
+        WeakCounter(self.counter.clone())
     }
 
     /// This method is inherently racey. Assume the count will have changed once
     /// the value is observed.
     #[inline]
     pub fn count(&self) -> usize {
-        self.0.load(Ordering::Acquire)
+        self.counter.load(Ordering::Acquire)
     }
 }
 
 impl Clone for Counter {
     fn clone(&self) -> Self {
-        self.0.fetch_add(1, Ordering::AcqRel);
-        Counter(self.0.clone())
+        self.counter.fetch_add(self.size, Ordering::AcqRel);
+        Counter { counter: self.counter.clone(), size: self.size }
     }
 }
 
 impl Drop for Counter {
     fn drop(&mut self) {
-        self.0.fetch_sub(1, Ordering::AcqRel);
+        self.counter.fetch_sub(self.size, Ordering::AcqRel);
     }
 }
 
@@ -88,9 +98,15 @@ impl WeakCounter {
     }
 
     /// Instead of clone + upgrade, this will only clone once
+    /// Defaults to a Counter of size 1
     pub fn spawn_upgrade(&self) -> Counter {
-        self.0.fetch_add(1, Ordering::AcqRel);
-        Counter(self.0.clone())
+        self.spawn_upgrade_with_size(1)
+    }
+
+    /// Instead of clone + upgrade, this will only clone once
+    pub fn spawn_upgrade_with_size(&self, size: usize) -> Counter {
+        self.0.fetch_add(size, Ordering::AcqRel);
+        Counter { counter: self.0.clone(), size }
     }
 }
 
@@ -114,5 +130,23 @@ mod tests {
         }
 
         assert_eq!(weak.count(), 0);
+    }
+
+    #[test]
+    fn different_sizes_work() {
+        let weak = WeakCounter::new();
+        assert_eq!(weak.count(), 0);
+
+        let counter = weak.spawn_upgrade_with_size(5);
+        assert_eq!(weak.count(), 5);
+
+        {
+            let _counter1 = counter.clone();
+            assert_eq!(weak.count(), 10);
+            let _counter2 = weak.spawn_upgrade();
+            assert_eq!(weak.count(), 11);
+        }
+
+        assert_eq!(weak.count(), 5);
     }
 }
