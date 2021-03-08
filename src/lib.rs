587b99ba72f6f644727ad68736b5eeb4c11828b8
Darren Tsung <darren.tsung@gmail.com>
2021-03-08 11:54:05 -0800

    Fix bug where Counter::new_with_size was not adding `size` to count
    Add additional docs and regression test

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-//! # raii-counter
 //! Rust type for a RAII Counter (counts number of held instances,
 //! decrements count on `Drop`), implemented with `Arc<AtomicUsize>`.
 //!
 //! Useful for tracking the number of holders exist for a handle,
 //! tracking the number of transactions that are in-flight, etc.
 //!
-//! ## Demo
+//! # Additional Features
+//! * Counters can have a size, eg. a Counter with `size` 4 adds 4
+//! to the count, and removes 4 when dropped.
+//!
+//! # Demo
 //!
 //! ```rust
 //! extern crate raii_counter;
@@ -32,7 +35,7 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 /// Essentially an AtomicUsize that is clonable and whose count is based
-/// on the number of copies. The count is automatically updated on Drop.
+/// on the number of copies (and their size). The count is automatically updated on Drop.
 #[derive(Debug)]
 pub struct Counter {
     counter: Arc<AtomicUsize>,
@@ -44,21 +47,30 @@ pub struct Counter {
 pub struct WeakCounter(Arc<AtomicUsize>);
 
 impl Counter {
+    /// Create a new Counter with the default size of 1.
     pub fn new() -> Counter {
         Counter::new_with_size(1)
     }
 
+    /// Create a new Counter with specified size. This counter adds
+    /// `size` to the count, and will remove `size` from the count
+    /// when dropped.
     pub fn new_with_size(size: usize) -> Counter {
         Counter {
-            counter: Arc::new(AtomicUsize::new(1)),
+            counter: Arc::new(AtomicUsize::new(size)),
             size,
         }
     }
 
-    /// Consume self (causing the count to decrease by 1)
+    /// Consume self (causing the count to decrease by `size`)
     /// and return a weak reference to the count through a WeakCounter
     pub fn downgrade(self) -> WeakCounter {
-        WeakCounter(self.counter.clone())
+        self.spawn_downgrade()
+    }
+
+    /// Create a new WeakCounter without consuming self.
+    pub fn spawn_downgrade(&self) -> WeakCounter {
+        WeakCounter(Arc::clone(&self.counter))
     }
 
     /// This method is inherently racey. Assume the count will have changed once
@@ -92,6 +104,8 @@ impl Drop for Counter {
 }
 
 impl WeakCounter {
+    /// Create a new WeakCounter. This WeakCounter creates a new count
+    /// with value: 0 since the WeakCounter has no effect on the count.
     pub fn new() -> WeakCounter {
         WeakCounter(Arc::new(AtomicUsize::new(0)))
     }
@@ -108,13 +122,14 @@ impl WeakCounter {
         self.spawn_upgrade()
     }
 
-    /// Instead of clone + upgrade, this will only clone once
-    /// Defaults to a Counter of size 1
+    /// Create a new Counter with `size` 1 without consuming the
+    /// current WeakCounter.
     pub fn spawn_upgrade(&self) -> Counter {
         self.spawn_upgrade_with_size(1)
     }
 
-    /// Instead of clone + upgrade, this will only clone once
+    /// Creates a new Counter with specified size without consuming the
+    /// current WeakCounter.
     pub fn spawn_upgrade_with_size(&self, size: usize) -> Counter {
         self.0.fetch_add(size, Ordering::AcqRel);
         Counter {
@@ -157,16 +172,27 @@ mod tests {
         let weak = WeakCounter::new();
         assert_eq!(weak.count(), 0);
 
-        let counter = weak.spawn_upgrade_with_size(5);
+        let counter5 = weak.spawn_upgrade_with_size(5);
         assert_eq!(weak.count(), 5);
 
         {
-            let _counter1 = counter.clone();
-            assert_eq!(weak.count(), 10);
-            let _counter2 = weak.spawn_upgrade();
-            assert_eq!(weak.count(), 11);
+            let _moved_counter5 = counter5;
+            assert_eq!(weak.count(), 5);
+            let _counter1 = weak.spawn_upgrade();
+            assert_eq!(weak.count(), 6);
         }
 
-        assert_eq!(weak.count(), 5);
+        assert_eq!(weak.count(), 0);
+    }
+
+    #[test]
+    fn counter_with_size_works() {
+        let counter = Counter::new_with_size(4);
+        assert_eq!(counter.count(), 4);
+
+        let weak = counter.spawn_downgrade();
+        assert_eq!(weak.count(), 4);
+        drop(counter);
+        assert_eq!(weak.count(), 0);
     }
 }
